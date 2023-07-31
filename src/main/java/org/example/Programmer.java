4a487372d74bbe9b037b5b65bfca347dcd6681bb
Methusela Fernando <136374272+methusela001@users.noreply.github.com>
2023-07-31 18:04:00 +0200

    Update Programmer.java

--- src/main/java/org/example/Programmer.java
+++ src/main/java/org/example/Programmer.java
@@ -1,7 +1,26 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Programmer {
+
+    private String name;
+    private int age;
+    private boolean wearsGlasses;
+
+    public Programmer(String name, int age, boolean wearsGlasses) {
+        this.name = name;
+        this.age = age;
+        this.wearsGlasses = wearsGlasses;
+    }
+    public void drinkCoffee(){
+        System.out.println("Espresso is the secret!");
+
+    }
+    public void printDetails(){
+        System.out.println( name + " is a " + age + "-yo programmer");
+    }
+    public boolean hasGlasses(){
+        System.out.println("Is Mario wearing glasses?");
+        return wearsGlasses;
+
     }
 }
